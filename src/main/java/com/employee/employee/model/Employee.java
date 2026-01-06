e0267cc07207ed0451d84097d58543bdc1c04791
Prince Mashava <mashavaprince09@gmail.com>
2026-01-06 20:17:51 +0200

    created Employee entity

---
+++ src/main/java/com/employee/employee/model/Employee.java
@@ -0,0 +1,60 @@
+package com.employee.employee.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email_address")
+    private String emailAddress;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String firstName, String lastName, String emailAddress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
