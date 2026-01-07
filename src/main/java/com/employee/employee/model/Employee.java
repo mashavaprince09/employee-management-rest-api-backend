68e1c7938e36ee0e52b841dbe9728044e96faebb
Prince Mashava <mashavaprince09@gmail.com>
2026-01-07 21:34:59 +0200

    changed int to Integer in the Employee Class

--- src/main/java/com/employee/employee/model/Employee.java
+++ src/main/java/com/employee/employee/model/Employee.java
@@ -8,7 +8,7 @@ import jakarta.persistence.*;
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
